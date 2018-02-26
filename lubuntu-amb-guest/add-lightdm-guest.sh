194e28340dc86a2fc27a9b7a5ebc854dea5033d9
mmxals <kttr@communia.org>
2018-02-26 10:32:03 +0100

    Update add-lightdm-guest.sh
    Fer entrada directe sense espera, i permet canviar d'usuari sense temps limit.

--- lubuntu-amb-guest/add-lightdm-guest.sh
+++ lubuntu-amb-guest/add-lightdm-guest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-echo -e "[Seat:*]\nallow-guest=true\nautologin-guest=true\nautologin-user-timeout=5" > /etc/lightdm/lightdm.conf.d/50-convidats.conf
+echo -e "[Seat:*]\nallow-guest=true\nautologin-guest=true\nautologin-user-timeout=0" > /etc/lightdm/lightdm.conf.d/50-convidats.conf
 #Afegim alguna configuració de l'usuari via skel, es podria tirar comandes via /etc/guest-session/prefs.sh, per autostarts: /etc/guest-session/auto.sh
 #mkdir /etc/guest-session
 #cp -pr /etc/skel /etc/guest-session/skel
